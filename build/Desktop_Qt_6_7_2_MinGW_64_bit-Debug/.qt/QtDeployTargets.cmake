d40f618d98e33ea4ee09d537b7425fbda8334347
frenchcokes <144563579+frenchcokes@users.noreply.github.com>
2024-08-29 14:00:42 -0600

    Initial commit

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Toil_FILE C:/Users/sydne/Documents/Toil/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/Toil.exe)
+set(__QT_DEPLOY_TARGET_Toil_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Toil_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
